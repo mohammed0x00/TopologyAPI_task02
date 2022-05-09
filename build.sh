86f7bc8c099ff0a8ef26bc2558188c8177baed4f
mohammed0x00 <mmansour.mm5@gmail.com>
2022-05-09 23:59:30 +0200

    Rearrange Files& Modify CMake

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cmake -S . -B build
 cd build
-cmake --build . && ./API_test
+cmake --build . && make test
--- test/API_test.cpp
+++ test/API_test.cpp
@@ -20,11 +20,13 @@ TEST(API, ReadJSONTest) {
 
 TEST(API, TopologyListSizeTest) {
 	/* Check the number of topologies */
+	API::Result res = API::readJSON((API::FileName)"InitializeTestFile.json");
 	EXPECT_EQ(API::queryTopologies().size, 1);
 }
 
 TEST(API, SetAndGetIDTest) {
 	/* Read topology ID, change it and read it again */
+	API::Result res = API::readJSON((API::FileName)"InitializeTestFile.json");
 	EXPECT_TRUE((string)API::queryTopologies().list[0]->getId() == "top1");
 	API::queryTopologies().list[0]->setId("topology2");
 	EXPECT_TRUE((string)API::queryTopologies().list[0]->getId() == "topology2");
@@ -32,12 +34,14 @@ TEST(API, SetAndGetIDTest) {
 
 TEST(API, QueryDevicesTest) {
 	/* Read list of devices in the topology "top1" */
+	API::Result res = API::readJSON((API::FileName)"InitializeTestFile.json");
 	EXPECT_TRUE((string)API::queryTopologies().list[0]->getDevices().list[0]->getID() == "res1");
 	EXPECT_TRUE((string)API::queryTopologies().list[0]->getDevices().list[1]->getID() == "m1");
 }
 
 TEST(API, SetAndGetDeviceIDTest) {
 	/* Read a device ID, change it and read it again */
+	API::Result res = API::readJSON((API::FileName)"InitializeTestFile.json");
 	EXPECT_TRUE((string)API::queryTopologies().list[0]->getDevices().list[0]->getID() == "res1");
 	API::queryTopologies().list[0]->getDevices().list[0]->setID("res2");
 	EXPECT_TRUE((string)API::queryTopologies().list[0]->getDevices().list[0]->getID() == "res2");
@@ -45,28 +49,32 @@ TEST(API, SetAndGetDeviceIDTest) {
 
 TEST(API, QueryDevicesAPITest) {
 	/* Query Devices in "topology2", read a device ID, change it and read it again */
-	EXPECT_TRUE((string)(API::queryDevices("topology2").list[0]->getID()) == "res2");
-	API::queryDevices("topology2").list[0]->setID((Device::DeviceIdType)"res3");
-	EXPECT_TRUE((string)(API::queryDevices("topology2").list[0]->getID()) == "res3");
+	API::Result res = API::readJSON((API::FileName)"InitializeTestFile.json");
+	EXPECT_TRUE((string)(API::queryDevices("top1").list[0]->getID()) == "res1");
+	API::queryDevices("top1").list[0]->setID((Device::DeviceIdType)"res3");
+	EXPECT_TRUE((string)(API::queryDevices("top1").list[0]->getID()) == "res3");
 }
 
 TEST(API, DeviceListSizeTest) {
 	/* Query Devices in "topology2" and read number of devices */
-	EXPECT_EQ(API::queryDevices("topology2").size, 2);
+	API::Result res = API::readJSON((API::FileName)"InitializeTestFile.json");
+	EXPECT_EQ(API::queryDevices("top1").size, 2);
 }
 
 TEST(API, QueryDevicesWithNetlistNode) {
 	/* Query Devices in "topology2" that are connected to "vin", read a device ID */
-	EXPECT_TRUE((string)(API::queryDevicesWithNetlistNode("topology2", "vin").list[0]->getID()) == "m1");
+	API::Result res = API::readJSON((API::FileName)"InitializeTestFile.json");
+	EXPECT_TRUE((string)(API::queryDevicesWithNetlistNode("top1", "vin").list[0]->getID()) == "m1");
 }
 
 TEST(API, WriteJSONTest) {
 	/* Write topology "topology2" to a .json file */
-	EXPECT_EQ(API::writeJSON((Topology::TopologyIdType)"topology2"), API::Done);
+	API::Result res = API::readJSON((API::FileName)"InitializeTestFile.json");
+	EXPECT_EQ(API::writeJSON((Topology::TopologyIdType)"top1"), API::Done);
 	
 	/* Test if the file was written correctly */
 	try {
-	        ifstream in1("topology2.json", ios::binary);
+	        ifstream in1("top1.json", ios::binary);
 	        ifstream in2("compareTestFile.json", ios::binary);
 	        
 	        EXPECT_TRUE(equalFiles(in1, in2));
